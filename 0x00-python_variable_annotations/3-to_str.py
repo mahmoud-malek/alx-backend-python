17de8b921a2aa647bf4a61e8654372468148b457
mahmoud malek <information.mahmoud@gmail.com>
2024-08-15 20:15:23 +0300

    fixed :hammer:

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-""" This module contains a function that returns the string representation of a float """
+""" This module contains a function that returns the string
+ representation of a float """
 
 
 def to_str(n: float) -> str:
--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,6 +2,6 @@
 """ This module contains a function that concatenates two strings """
 
 
-def sum_list(input: list) -> float:
+def sum_list(input_list: list[int]) -> float:
     """ This function returns the sum of a list of floats """
     return sum(input)
