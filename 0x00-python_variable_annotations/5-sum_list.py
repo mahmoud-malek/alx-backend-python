a4fc57db05997509a376400f42424eeed0bc5d7a
mahmoud malek <information.mahmoud@gmail.com>
2024-08-15 20:16:32 +0300

    fixedagain :hammer:

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,6 +2,6 @@
 """ This module contains a function that concatenates two strings """
 
 
-def sum_list(input_list: list[int]) -> float:
-    """ This function returns the sum of a list of floats """
-    return sum(input)
+def sum_list(input_list: list[float]) -> float:
+    """ This function concatenates two strings """
+    return sum(input_list)
