25f83e3c3fb6eb624b524bf0fb874b424144a9ff
mahmoud malek <information.mahmoud@gmail.com>
2024-08-15 20:52:44 +0300

    try

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+""" a module contain sum_mixed function that takes a list
+and return their sum """
+
+from typing import Union
+
+
+def sum_mixed_list(mxd_list: Union[int, float]):
+    """ a function to return the sum values """
+    return sum(mxd_list)
