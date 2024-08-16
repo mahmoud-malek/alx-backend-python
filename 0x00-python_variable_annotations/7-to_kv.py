8c03056485dad123f5f305da300fff0abcdb3417
mahmoud malek <information.mahmoud@gmail.com>
2024-08-16 04:22:33 +0300

    finishagian :hammer:

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -3,9 +3,9 @@
 """ This module contains a function that takes a list of
  floats and returns their sum """
 
-from typing import List, Union, Tuple
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[int, Union[int, float]]:
-    """ This function takes a float and returns a tuple """
-    return (k, v ** 2)
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[int, float]]:
+    """ This function takes a list of floats and returns their sum """
+    return (k, v * v)
