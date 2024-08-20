4ca44a40e2194494ef27ce7005b0a838b5a0c0f4
mahmoud malek <information.mahmoud@gmail.com>
2024-08-20 04:30:22 +0300

    doneSecond :hammer:

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+""" an asynchronous coroutine that takes in an integer """
+
+import asyncio
+import random
+from typing import List
+
+rait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """ an asynchronous coroutine that takes in an integer
+    argument (max_delay, with a default value of 10) named wait_random
+    that waits for a random delay between 0 and max_delay
+    (included and float value) seconds and eventually returns it.
+    """
+    delayes = [await wait_random(max_delay) for _ in range(n)]
+    return sorted(delayes)
