97cd19b44bb53b279d3ac00fdbc2e1414152a11d
mahmoud malek <information.mahmoud@gmail.com>
2024-09-09 18:46:08 +0300

    add\udpate files :fire:

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,10 @@
 """Generic utilities for github org client.
 """
 from utils import access_nested_map
+from utils import get_json
+from utils import memoize
 import unittest
+from unittest import mock
 from parameterized import parameterized
 from typing import (
     Union,
@@ -45,6 +48,42 @@ class TestAccessNestedMap(unittest.TestCase):
         """ test for key error exception """
         with self.assertRaises(expected):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """ a suit of testing for GetJson method """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(self, url: str, payload: Dict) -> None:
+        """ a method to test the getjson """
+        with mock.patch('requests.get') as mock_get:
+            mock_get.return_value.json.return_value = payload
+            self.assertEqual(get_json(url), payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """ a suit of testing for memoize method """
+
+    def test_memoize(self):
+        """ a method to test the memoize """
+
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with mock.patch.object(TestClass, 'a_method') as mock_a_method:
+            mycls = TestClass()
+            mycls.a_property
+            mycls.a_property
+            mock_a_method.assert_called_once()
 
 
 if __name__ == '__main__':
